efda607456ed8246ab55ce1edfa39dfd93127f93
alexgryzlov <gryzlov.ad@yandex.ru>
2019-02-18 20:10:43 +0300

    add a new solution to the same file names problem

---
+++ task1-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+path=$1
+archivename=$2
+shift 2
+mkdir "$path"
+for format in $@
+do
+    find ~/ -depth -name "*.$format" -exec cp --force --backup=numbered {} "$path" \;
+done
+tar -czf "$archivename" "$path" &> /dev/null
+echo "done"
+ 
+
