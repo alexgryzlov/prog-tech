acd2f9f265584607c2ca6968e10e6bfc16f21f14
alexgryzlov <gryzlov.ad@yandex.ru>
2019-02-18 19:05:18 +0300

    add multiple format search

--- task1-1.sh
+++ task1-1.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-# programming technology - first task
 path=$1
 archivename=$2
-shift
-shift
-for format in "$@"
+shift 2
+mkdir "$path"
+for format in $@
 do
-	IFS=$'\n'
-	codefiles=$(find ~/ -depth -name "*.$format")
-	for var in $codefiles
-	do
-		cp $var $path
-	done
-	IFS=' '
+    find ~/ -depth -name "*.$format" -exec cp {} "$path" \; 
 done
-IFS=$'\n'
-tar -czf $archivename  $path
+tar -czf "$archivename" "$path" &> /dev/null 
 echo "done"
-
 
