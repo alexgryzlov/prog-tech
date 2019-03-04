5f130e42229b91e3618ed6c09806b81b39e2c331
alexgryzlov <gryzlov.ad@yandex.ru>
2019-03-04 14:26:20 +0300

    first working version

---
+++ A/preparing.py
@@ -0,0 +1,1 @@
+open("./index.h", 'w').write("#include <iostream>\n#include <vector>\n")
---
+++ C/main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+int main() {
+    std::vector<int> my_vector;
+    my_vector.push_back(1);
+    my_vector.push_back(2);
+    std::cout << my_vector[0] << ' ' << my_vector[1] << std::endl;
+    return 0;
+}
